705bcc45fd3628e4740df5a4d6fb258a322a7630
Rodrigo Bermudez Schettino <rodrigobermudezschettino@gmail.com>
2019-05-25 19:15:31 +0800

    Follow Google's Shell Style Guide

--- meta-laptop.sh
+++ meta-laptop.sh
@@ -5,16 +5,27 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-set -o errtrace
 
-git clone "https://github.com/$GITHUB_USER/dotfiles-local" ~/dotfiles-local
+fetch_local_dotfiles() {
+  git clone "https://github.com/$GITHUB_USER/dotfiles-local" ~/dotfiles-local
+}
 
-# 2. thoughtbot/dotfiles
-chsh -s "$(command -v zsh)"
-git clone git://github.com/thoughtbot/dotfiles.git ~/dotfiles
-env RCRC="$HOME/dotfiles/rcrc" rcup
+install_dotfiles() {
+  chsh -s "$(command -bv zsh)"
+  git clone git://github.com/thoughtbot/dotfiles.git ~/dotfiles
+  env RCRC="$HOME/dotfiles/rcrc" rcup
+}
 
-# 3. thoughtbot/laptop
-mkdir -p "$DEV_DIR" && cd "$DEV_DIR"
-curl --remote-name https://raw.githubusercontent.com/thoughtbot/laptop/master/mac
-sh mac 2>&1 | tee ~/laptop.log
+install_laptop() {
+  mkdir -p "$DEV_DIR" && cd "$DEV_DIR"
+  curl --remote-name https://raw.githubusercontent.com/rodrigobdz/laptop/master/mac
+  sh mac 2>&1 | tee ~/laptop.log
+}
+
+main() {
+  fetch_local_dotfiles
+  install_dotfiles
+  install_laptop
+}
+
+main "$@"
