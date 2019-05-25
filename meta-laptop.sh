d8bd6f794e0430f05fc9f62c71821c3e3e276c43
Rodrigo Bermudez Schettino <rodrigobermudezschettino@gmail.com>
2019-05-25 19:15:20 +0800

    Lint script

--- meta-laptop.sh
+++ meta-laptop.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
+#
+# Download and install laptop script and (local) dotfiles.
 
 set -o errexit
 set -o pipefail
 set -o nounset
 set -o errtrace
 
-# 1. custom dotfiles-local
-git clone https://github.com/$GITHUB_USER/dotfiles-local ~/dotfiles-local
+git clone "https://github.com/$GITHUB_USER/dotfiles-local" ~/dotfiles-local
 
 # 2. thoughtbot/dotfiles
-chsh -s $(which zsh)
+chsh -s "$(command -v zsh)"
 git clone git://github.com/thoughtbot/dotfiles.git ~/dotfiles
-env RCRC=$HOME/dotfiles/rcrc rcup
+env RCRC="$HOME/dotfiles/rcrc" rcup
 
 # 3. thoughtbot/laptop
-mkdir -p $DEV_DIR && cd $DEV_DIR
+mkdir -p "$DEV_DIR" && cd "$DEV_DIR"
 curl --remote-name https://raw.githubusercontent.com/thoughtbot/laptop/master/mac
 sh mac 2>&1 | tee ~/laptop.log
-
